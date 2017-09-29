b2863a44da1419b38d4151753ecf09ccee53b8b9
Lucas Polonio <lucas.polonio@thescore.com>
2017-09-29 18:36:27 -0400

    transform string to 16 elements list using md5

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,14 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
   """
 
-  @doc """
-  Hello world.
+  def main(input) do
+    input
+    |> hash_input
+  end
 
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
